d72d4656aac4d977dc6508e60757c9c7eeeecf23
mavkrdof <34178589+mavkrdof@users.noreply.github.com>
2024-07-17 14:31:47 +1000

    create backend_manager

--- peertopeermessagingapp/src/peertopeermessagingapp/app.py
+++ peertopeermessagingapp/src/peertopeermessagingapp/app.py
@@ -7,6 +7,7 @@ import toga
 import toga.style
 import toga.style.pack
 from peertopeermessagingapp.graphical_user_interface import GUI_manager
+from peertopeermessagingapp.backend import Backend_manager
 
 
 class PeertoPeerMessagingApp(toga.App):
@@ -17,8 +18,10 @@ class PeertoPeerMessagingApp(toga.App):
             None
         Returns: None
         """
+        # initialise Backend
+        self.backend = Backend_manager(app=self)
+        # initialise GUI
         GUI = GUI_manager(app=self)
-
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = GUI.main_box
         GUI.start()
---
+++ peertopeermessagingapp/src/peertopeermessagingapp/backend.py
@@ -0,0 +1,4 @@
+class Backend_manager:
+    def __init__(self, app) -> None:
+        self.app = app
+        self.user_data = None
